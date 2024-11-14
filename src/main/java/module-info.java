82a044d1d1963ac3333ce2f82a75447a7932a2c5
WanAdamm <wanadamworkmail@gmail.com>
2024-11-14 19:07:33 +0800

    layout for homepage and new styling file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,10 @@ module com.example {
 
     requires transitive javafx.graphics;
 
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.bootstrapicons;
+
     opens com.example to javafx.fxml;
     exports com.example;
 }
