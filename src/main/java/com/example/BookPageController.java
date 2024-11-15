f85d06393c9e516b1912784a9f503611a25c92bf
WanAdamm <wanadamworkmail@gmail.com>
2024-11-15 22:34:23 +0800

    done homepage

--- src/main/java/com/example/BookPageController.java
+++ src/main/java/com/example/BookPageController.java
@@ -3,7 +3,7 @@ package com.example;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class BookPageController {
 
     @FXML
     private void switchToPrimary() throws IOException {
--- src/main/java/com/example/HomePageController.java
+++ src/main/java/com/example/HomePageController.java
@@ -6,6 +6,11 @@ import javafx.fxml.FXML;
 public class HomePageController {
 
     @FXML
+    private void switchToBookPage() throws IOException {
+        App.setRoot("book");
+    }
+
+    @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
